a81af66b83dbb7bab586aee0c911c5f31ea3c0d8
Daniel Sobieraj <Daniel.Sobieraj@onexgroup.pl>
2022-03-10 23:21:52 +0100

    add windowHelper & change display navMenu

--- src/components/nav-menu/NavMenu.tsx
+++ src/components/nav-menu/NavMenu.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { useWindowSize } from '../../common/helpers/windowHelper';
+import { MEDIUM_DEVICE } from '../../common/variables/breakpoints';
 import { menuItems } from './menuItems';
 import './NavMenu.scss';
 
 const NavMenu = () => {
+    const size = useWindowSize();
+
     return (
-        <nav>
-            <div className="d-none d-md-flex justify-content-between align-items-center">
-                {menuItems.map(({ name }, index) => (
-                    <span key={index} className="menu--item">
-                        {name}
-                    </span>
-                ))}
-            </div>
-            <div className="d-md-none d-flex justify-content-between align-items-center">
-                {menuItems.slice(0, 4).map(({ name }, index) => (
-                    <span key={index} className="menu--item">
-                        {name}
-                    </span>
-                ))}
-            </div>
+        <nav className="d-flex justify-content-between align-items-center">
+            {size.width > MEDIUM_DEVICE ? (
+                <>
+                    {menuItems.map(({ name }, index) => (
+                        <span key={index} className="menu--item">
+                            {name}
+                        </span>
+                    ))}
+                </>
+            ) : (
+                <>
+                    {menuItems.slice(0, 4).map(({ name }, index) => (
+                        <span key={index} className="menu--item">
+                            {name}
+                        </span>
+                    ))}
+                </>
+            )}
         </nav>
     );
 };
