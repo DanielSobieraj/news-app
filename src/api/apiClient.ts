85b887d14d1a5826846ffeee978127ea824964c5
Daniel <danielsobieraj7@gmail.com>
2022-10-24 23:21:33 +0200

    CORS check

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -10,7 +10,13 @@ const { REACT_APP_API_BASE_URL, REACT_APP_NEWS_APP_API_KEY } = process.env;
 // eslint-disable-next-line @typescript-eslint/ban-types
 const apiClient = (endpoint: string, data?: {}) =>
     fetch(`${REACT_APP_API_BASE_URL}/${endpoint}?` + new URLSearchParams(data), {
-        headers: { 'X-API-KEY': REACT_APP_NEWS_APP_API_KEY ?? '' },
+        headers: {
+            'X-API-KEY': REACT_APP_NEWS_APP_API_KEY ?? '',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTION',
+        },
     });
 
 export const getMainArticleRequest = async (
